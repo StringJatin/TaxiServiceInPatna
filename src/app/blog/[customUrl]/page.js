cb0d9962ffa040403174e960deb12b2beb0463a7
Jatin chouhan <jatinchouhan1515@gmail.com>
2023-09-07 19:19:07 +0530

    Blog page

--- src/app/blog/[customUrl]/page.js
+++ src/app/blog/[customUrl]/page.js
@@ -15,43 +15,25 @@ async function getData(customUrl) {
   return res.json();
 }
 
-
 const BlogPost = async ({ params }) => {
   const data = await getData(params.customUrl);
-  console.log(data);
+  const postInfo = data;
   return (
     <div className={styles.container}>
-      <div className={styles.top}>
-        <div className={styles.info}>
-          <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>
-            {data.desc}
-          </p>
-          <div className={styles.author}>
-            <Image
-              src={data.img}
-              alt=""
-              width={40}
-              height={40}
-              className={styles.avatar}
-            />
-            <span className={styles.username}>{data.username}</span>
-          </div>
+      <div className={styles.postPage}>
+        <h1>{postInfo.title}</h1>
+        <div>
+          {postInfo.metadescription}
         </div>
-        <div className={styles.imageContainer}>
-          <Image
-            src={data.img}
-            alt=""
-            fill={true}
-            className={styles.image}
-          />
+        <a href="" className="author">
+          {" "}
+          by {postInfo.author}{" "}
+        </a>
+        <div className={styles.imagePost}>
+          <img src={`https://backend-taxi.onrender.com/${postInfo.cover}`} />
         </div>
+        <div dangerouslySetInnerHTML={{ __html: postInfo.content }} />
       </div>
-      <div className={styles.content}>
-        <p className={styles.text}>
-         {data.content}
-        </p>
-      </div> 
     </div>
   );
 };
