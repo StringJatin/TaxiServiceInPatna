fe3f03fd1f29be3d9d2357f8126e7d8aa17cf4f7
Jatin chouhan <jatinchouhan1515@gmail.com>
2023-09-07 12:45:02 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   next.config.js
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   public/metaData.json
    	modified:   src/app/aboutUs/page.js
    	new file:   src/app/blogs/blogView.js
    	new file:   src/app/blogs/page.js
    	new file:   src/app/blogs/page.module.css
    	modified:   src/app/globals.css
    	modified:   src/app/layout.js
    	modified:   src/app/page.js
    	modified:   src/components/Dashboard/Dashboard.js
    	new file:   src/components/Dashboard/tables/CarPackage.js
    	new file:   src/components/Dashboard/tables/Local.js
    	new file:   src/components/Dashboard/tables/OneWayTrip.js
    	new file:   src/components/Dashboard/tables/RoundTrip.js
    	new file:   src/components/Dashboard/tables/tables.module.css
    	new file:   src/components/Loading/Loading.js
    	new file:   src/components/Loading/Loading.module.css
    	modified:   src/components/Navbar/navbar.js
    	new file:   src/pages/404.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['mixkit.imgix.net'], // Add the hostname(s) for your images
+      domains: ['mixkit.imgix.net'], 
+      domains : ['backend-taxi.onrender.com'],
+       // Add the hostname(s) for your images
     },
     // Other Next.js configuration options, if needed
   };
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,34 +3,39 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BiBus, BiDirections, BiHome, BiPackage } from 'react-icons/bi';
 import styles from './dashboard.module.css';
+import RoundTrip from './tables/RoundTrip';
+import OneWayTrip from './tables/OneWayTrip';
+import Local from './tables/Local';
+import CarPackage from './tables/CarPackage';
 
 const Dashboard = () => {
   const [selectedOption, setSelectedOption] = useState('');
   const [fields, setFields] = useState([]);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  
   useEffect(() => {
     async function fetchData() {
-        try {
-            console.log("Fetching data...");
-            const res = await fetch("http://localhost:3000/api/getFormdata", {
-                cache: "no-store",
-            });
-            if (!res.ok) {
-                console.error("Failed to fetch data. Rsponse status:", res.status);
-                // Log the response status to see if it provides more information
-                throw new Error("Failed to fetch data");
-            }
-            const data = await res.json();
-            setData(data);
-            setLoading(false);
-        } catch (error) {
-            console.error("Error fetching form data:", error);
+      try {
+        console.log("Fetching data...");
+        const res = await fetch("http://localhost:3000/api/formdata", {
+          cache: "no-store",
+        });
+        if (!res.ok) {
+          console.error("Failed to fetch data. Response status:", res.status);
+          // Log the response status to see if it provides more information
+          throw new Error("Failed to fetch data");
         }
+        const data = await res.json();
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching form data:", error);
+      }
     }
 
     fetchData();
-}, []);
+  }, []);
 
   const options = [
     { icon: <BiBus size={30} />, label: 'Round trip' },
@@ -42,6 +47,7 @@ const Dashboard = () => {
   const handleOptionClick = (label) => {
     setSelectedOption(label);
 
+    
     // Map the selected option to its corresponding fields
     const optionToFields = {
       'Round trip': ['S.No', 'From', 'To', 'Date', 'Time', 'Phone', 'ReturnDate', 'CarType'],
@@ -51,8 +57,10 @@ const Dashboard = () => {
     };
 
     setFields(optionToFields[label]);
+
   };
-  console.log("Fields:", fields);
+
+
 
   return (
     <div className={styles.dashboard}>
@@ -71,26 +79,24 @@ const Dashboard = () => {
         </ul>
       </div>
       <div className={styles.content}>
-        <div className={styles.tableContainer}>
-          <table className={styles.dataTable}>
-            <thead>
-              <tr>
-                {fields.map((field, index) => (
-                  <th key={index}>{field}</th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((row, rowIndex) => (
-                <tr key={rowIndex}>
-                  {fields.map((field, fieldIndex) => (
-                    <td key={fieldIndex}>{row[field]}</td>
-                  ))}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        {loading ? (
+          <p>Loading data...</p>
+        ) : (
+          <div className={styles.tableContainer}>
+            {/* Table components */}
+
+            { selectedOption === 'Round trip' && <RoundTrip data={data} setData={setData} /> }
+            { selectedOption === 'Oneway trip' && <OneWayTrip data={data} setData={setData} />}
+            {selectedOption === 'Local' && <Local data={data} setData={setData} /> }
+            { selectedOption === 'Car package' && <CarPackage data={data} setData={setData} />}
+
+
+
+
+
+
+          </div>
+        )}
       </div>
     </div>
   );
