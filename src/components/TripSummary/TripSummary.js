b4da94dfba59b91f7735e94667a578349d0f8170
AYsCReation <ayushm850@gmail.com>
2023-09-12 19:34:16 +0530

    Changes to be committed: 	modified:   src/components/TripSummary/TripSummary.js

--- src/components/TripSummary/TripSummary.js
+++ src/components/TripSummary/TripSummary.js
@@ -35,6 +35,9 @@ const TripSummary = ({
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+         
+            'Access-Control-Allow-Origin': '*',
+         
         },
         body: JSON.stringify({
           activeMenu,
