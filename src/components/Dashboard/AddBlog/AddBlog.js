9970ac07ffc29d760082550625cb7f3cd366dbb1
AYsCReation <ayushm850@gmail.com>
2023-09-12 21:01:30 +0530

    Changes to be committed: 	modified:   src/components/Dashboard/AddBlog/AddBlog.js

--- src/components/Dashboard/AddBlog/AddBlog.js
+++ src/components/Dashboard/AddBlog/AddBlog.js
@@ -54,7 +54,7 @@ const AddBlog = () => {
          const mediaUrl =  await imageUpload()
          console.log("media" , mediaUrl)
       // Send POST request to backend API
-      const res = await fetch(`/api/createPost`, {
+      const res = await fetch(`https://taxiapi-production.up.railway.app/api/createPost`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
