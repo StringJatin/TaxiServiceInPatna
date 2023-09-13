cea5546c13614576efadda8efdaa6e4cec93021c
Avirup Banik <avirup.banik2017@gmail.com>
2023-09-13 22:29:36 +0530

    minor changes

--- src/app/api/getPosts/route.js
+++ src/app/api/getPosts/route.js
@@ -1,19 +1,19 @@
 import dbConn from "@/utils/dbConn";
 import PostPage from "@/models/post";
-import {NextResponse} from "next/server";
+import { NextResponse } from "next/server";
 
-export const revalidate = 10;
+export const revalidate = 1;
 export const fetchCache = 'force-no-store';
 
-export const GET = async (request) => { 
-  
-    try {
-      await dbConn();
-  
-      const posts = await PostPage.find({});
-      
-      return new NextResponse(JSON.stringify(posts), { status: 200 });
-    } catch (err) {
-      return new NextResponse("Database Error", { status: 500 });
-    }
-  };
+export const GET = async (request) => {
+
+  try {
+    await dbConn();
+
+    const posts = await PostPage.find({});
+
+    return new NextResponse(JSON.stringify(posts), { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
