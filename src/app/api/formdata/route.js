87f81d4a555e58eb26c760cfdf02768034adb757
Avirup Banik <avirup.banik2017@gmail.com>
2023-09-13 18:28:52 +0530

    minor changes

--- src/app/api/formdata/route.js
+++ src/app/api/formdata/route.js
@@ -1,38 +1,44 @@
-
-import dbConn from "@/utils/dbConn";
-import FormData from "@/models/form";
 import { NextResponse } from "next/server";
 
-export const GET = async (req) => {
+// export const GET = async (request) => {
+
+//     try {
+//         return new Response(JSON.stringify({message: "hola"}), { status: 200 })
+//     } catch (err) {
+//         return new NextResponse("Database Error", { status: 500 });
+//     }
+// };
+export const GET = async (request) => {
+
     try {
         return new Response(JSON.stringify({message: "hola"}), { status: 200 })
     } catch (err) {
         return new NextResponse("Database Error", { status: 500 });
     }
-}
+};
 
-export const POST = async(req, res) => {
-    try {
+// export const POST = async(req, res) => {
+//     try {
 
-        const body = await req.json();
-        await dbConn();
+//         const body = await req.json();
+//         await dbConn();
 
-        await FormData.create(body);
+//         await FormData.create(body);
 
-        return NextResponse.json({
-            message: "Message sent successfully!"
-        }, {
-            status: 200
-        }
-        )
+//         return NextResponse.json({
+//             message: "Message sent successfully!"
+//         }, {
+//             status: 200
+//         }
+//         )
 
-    } catch (e) {
-        return NextResponse.json(
-            { message: "Server error, please try again!" },
-            { status: 500 }
-        )
-    }
-}
+//     } catch (e) {
+//         return NextResponse.json(
+//             { message: "Server error, please try again!" },
+//             { status: 500 }
+//         )
+//     }
+// }
 
 
 
