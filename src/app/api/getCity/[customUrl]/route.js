060c26cb666ce0db31195cc239ab7eb293aef4f4
Avirup Banik <81384443+Avirup-001@users.noreply.github.com>
2023-09-14 22:25:14 +0530

    update city api changes

--- src/app/api/getCity/[customUrl]/route.js
+++ src/app/api/getCity/[customUrl]/route.js
@@ -24,12 +24,14 @@ export const DELETE = async (request, { params }) => {
     // const deletePost = PostPage.find((post) => post._id === parseInt(id))
     const res = await CityData.deleteOne(id);
 
-    return new NextResponse("Post has been deleted", { status: 200 ,
+    return new NextResponse("Post has been deleted", {
+      status: 200,
       headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        }, });
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    });
   } catch (err) {
     return new NextResponse("Database Error", { status: 500 });
   }
@@ -41,6 +43,43 @@ export const PUT = async (request, { params }) => {
   try {
     await dbConn();
     const newdata = await request.json();
+    // const {
+    //   Newmetatitle,
+    //   Newmetadescription,
+    //   Newkeywords,
+    //   Newtitle,
+    //   Newcontent,
+    //   NewfootTitle,
+    //   NewcustomUrl,
+    //   Newfaq1Ques,
+    //   Newfaq1Ans,
+    //   Newfaq2Ques,
+    //   Newfaq2Ans,
+    //   Newfaq3Ques,
+    //   Newfaq3Ans,
+    //   Newfaq4Ques,
+    //   Newfaq4Ans,
+    //   Newfaq5Ques,
+    //   Newfaq5Ans,
+    //   NewmediaUrl
+    // } = newdata;
+    // const post = await CityData.updateMany({ _id: id }, {
+    //   $set: {
+    //     metatitle: Newmetatitle,
+    //     metadescription: Newmetadescription,
+    //     keywords: Newkeywords,
+    //     title: Newtitle,
+    //     content: Newcontent,
+    //     footTitle: NewfootTitle,
+    //     customUrl: NewcustomUrl,
+    //     mediaUrl: NewmediaUrl,
+    //     faq1: { que: Newfaq1Ques, ans: Newfaq1Ans },
+    //     faq2: { que: Newfaq2Ques, ans: Newfaq2Ans },
+    //     faq3: { que: Newfaq3Ques, ans: Newfaq3Ans },
+    //     faq4: { que: Newfaq4Ques, ans: Newfaq4Ans },
+    //     faq5: { que: Newfaq5Ques, ans: Newfaq5Ans },
+    //   }
+    // });
     const {
       Newmetatitle,
       Newmetadescription,
@@ -61,33 +100,35 @@ export const PUT = async (request, { params }) => {
       Newfaq5Ans,
       NewmediaUrl
     } = newdata;
-    const post = await CityData.updateMany({_id : id},{ $set: {
-      metatitle: Newmetatitle,
-      metadescription: Newmetadescription,
-      keywords: Newkeywords,
-      title: Newtitle,
-      content: Newcontent,
-      footTitle: NewfootTitle,
-      customUrl: NewcustomUrl,
-      mediaUrl:NewmediaUrl,
-      faq1Ques: Newfaq1Ques,
-      faq1Ans: Newfaq1Ans,
-      faq2Ques: Newfaq2Ques,
-      faq2Ans: Newfaq2Ans,
-      faq3Ques: Newfaq3Ques,
-      faq3Ans: Newfaq3Ans,
-      faq4Ques: Newfaq4Ques,
-      faq4Ans: Newfaq4Ans,
-      faq5Ques: Newfaq5Ques,
-      faq5Ans: Newfaq5Ans,
-    }});
+    
+    const filter = { _id: id };
+    
+    const update = {
+      $set: {
+        metatitle: Newmetatitle,
+        metadescription: Newmetadescription,
+        keywords: Newkeywords,
+        title: Newtitle,
+        content: Newcontent,
+        footTitle: NewfootTitle,
+        customUrl: NewcustomUrl,
+        mediaUrl: NewmediaUrl,
+        faq1: { que: Newfaq1Ques, ans: Newfaq1Ans },
+        faq2: { que: Newfaq2Ques, ans: Newfaq2Ans },
+        faq3: { que: Newfaq3Ques, ans: Newfaq3Ans },
+        faq4: { que: Newfaq4Ques, ans: Newfaq4Ans },
+        faq5: { que: Newfaq5Ques, ans: Newfaq5Ans },
+      }
+    };
+    
+    const options = { new: true }; // To return the updated document
+    
+    const post = await CityData.updateOne(filter, update, options);
+    
 
-    return new NextResponse(JSON.stringify(post), { status: 200 ,
-      headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        }, });
+    return new NextResponse(JSON.stringify(post), {
+      status: 200
+    });
   } catch (err) {
     return new NextResponse("Database Error", { status: 500 });
   }
--- src/models/city.js
+++ src/models/city.js
@@ -1,57 +1,57 @@
 const mongoose = require('mongoose');
 const citySchema = new mongoose.Schema({
-    metatitle: {
-        type: String,
-        required: true,
-      },
-      metadescription: {
-        type: String,
-        required: true,
-      }, 
-      keywords: {
-        type: String,
-        required: true,
-      },
-    title: {
-      type: String,
-      required: true,
-    },
-    footTitle:{
-      type: String,
-      required: true,
-    },
-    content: {
-      type: String,
-      required: true,
-    },
-    customUrl :{
-      type: String,
-    },
-    mediaUrl:{
-      type:String,
-      required:true
+  metatitle: {
+    type: String,
+    required: true,
   },
-    faq1: {
-      que : String,
-      ans : String,
-    },
-    faq2: {
-      que : String,
-      ans : String,
-    },
-    faq3: {
-      que : String,
-      ans : String,
-    },
-    faq4: {
-      que : String,
-      ans : String,
-    },
-    faq5: {
-      que : String,
-      ans : String,
-    }
-  });
-  const CityData = mongoose.models.CityData ||  mongoose.model('CityData', citySchema);
+  metadescription: {
+    type: String,
+    required: true,
+  },
+  keywords: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  footTitle: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  customUrl: {
+    type: String,
+  },
+  mediaUrl: {
+    type: String,
+    required: true
+  },
+  faq1: {
+    que: String,
+    ans: String,
+  },
+  faq2: {
+    que: String,
+    ans: String,
+  },
+  faq3: {
+    que: String,
+    ans: String,
+  },
+  faq4: {
+    que: String,
+    ans: String,
+  },
+  faq5: {
+    que: String,
+    ans: String,
+  }
+});
+const CityData = mongoose.models.CityData || mongoose.model('CityData', citySchema);
 
-  export default  CityData;
+export default CityData;
